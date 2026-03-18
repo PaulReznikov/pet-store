87290b8a30cc2e439e22cad60c61b9fe06546b5b
Oliver1ck <artem131103@yandex.ru>
2026-03-18 14:13:09 +0300

    feat: set up database schema and migration scripts for pet store

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
 include .env
 export
+
+export ROOT=$(shell pwd)
+
+db-up:
+	@docker compose up -d db
+
+db-down:
+	@docker compose down db
+
+migrations-up:
+	@docker compose up --build migrator
+
+migrate-down:
+	@docker compose run --rm migrator ./migrator-app down
+
+pgadmin-up:
+	@docker compose up -d pgadmin
+
+pgadmin-down:
+	@docker compose down pgadmin
---
+++ migrations/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"os"
+
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed scripts/*.sql
+var embedMigrations embed.FS
+
+func main() {
+	goose.SetBaseFS(embedMigrations)
+
+	dbString := os.Getenv("DATABASE_URL")
+	if dbString == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	db, err := sql.Open("pgx", dbString)
+	if err != nil {
+		log.Fatalf("Failed to open DB: %v\n", err)
+	}
+	defer db.Close()
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatalf("Failed to set dialect: %v\n", err)
+	}
+
+	command := "up"
+	if len(os.Args) > 1 {
+		command = os.Args[1]
+	}
+
+	log.Printf("Running goose command: '%s'...\n", command)
+
+	if err := goose.Run(command, db, "scripts"); err != nil {
+		log.Fatalf("goose %s failed: %v\n", command, err)
+	}
+	
+	log.Println("Migration command completed successfully!")
+}
