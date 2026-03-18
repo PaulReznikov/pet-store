addb1ffbe71acbb0d63c2dad4a5fee232193b775
Oliver1ck <artem131103@yandex.ru>
2026-03-18 14:12:54 +0300

    Fix:  productPackages type, Product type. and add product category

--- internal/core/product.go
+++ internal/core/product.go
@@ -5,15 +5,23 @@ import "time"
 type Product struct {
   ID int `json:"id"`
   Name string `json:"name"`
+  Category ProductCategory `json:"category"`
   ImgURL string `json:"img_url"`
   Description string `json:"description"`
   Price float64 `json:"price"`
-  Sale ProductSale `json:"sale"`
+  Sale *ProductSale `json:"sale,omitempty"`
   Animal Animal `json:"animal"`
   Brand Brand `json:"brand"`
   Packages []ProductPackages `json:"packages"`
   CreatedAt time.Time `json:"created_at"`
   UpdatedAt time.Time `json:"updated_at"`
+}
+
+
+type ProductCategory struct {
+  ID int `json:"id"`
+  Name string `json:"name"`
+  ParentID *int `json:"parent_id,omitempty"`
 }
 
 type ProductUnit string
@@ -26,20 +34,25 @@ const (
   UnitPcs ProductUnit = "шт"
 )
 
+
 type ProductPackages struct {
   ID int `json:"id"`
   ProductID int `json:"product_id"`
-  Value int `json:"quantity"`
+  // Value - фактическое значение фасовки
+  Value float64 `json:"value"`
+  // Unit - единица измерения
   Unit ProductUnit `json:"unit"`
+  // Count - количество товара в наличии для данной фасовки
   Count int `json:"count"`
+  // Price - цена товара в данной фасовке
+  Price float64 `json:"price"`
 }
 
 
 type ProductSale struct {
   ID int `json:"id"`
-  ProductID int `json:"product_id"`
-  Quantity int `json:"quantity"`
-  TotalPrice float64 `json:"total_price"`
+  Name string `json:"name"`
+  SalePercent float64 `json:"sale_percent"`
   StartSale time.Time `json:"start_sale"`
   EndSale time.Time `json:"end_sale"`
 }
