04f8406af0a25c133919927b761c8b05f7ede2e6
Oliver1ck <artem131103@yandex.ru>
2026-03-23 17:42:25 +0300

    remove logger in cmd

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/PaulReznikov/pet-store/internal/config"
-	"github.com/PaulReznikov/pet-store/pkg/logger"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	logger.Init()
 
 	_, err := config.LoadConfig()
 	if err != nil {
