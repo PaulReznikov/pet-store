9c952d45e85c6ac00448f8bfe78767a4d825706f
Oliver1ck <artem131103@yandex.ru>
2026-03-23 17:23:58 +0300

    refactor: enhance logging and error handling in main migration logic; load DB connection string from config

--- migrations/main.go
+++ migrations/main.go
@@ -1,48 +1,67 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"embed"
-	"log"
 	"os"
 
-	"database/sql"
-
+	"github.com/PaulReznikov/pet-store/internal/config"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 //go:embed scripts/*.sql
 var embedMigrations embed.FS
 
 func main() {
-	goose.SetBaseFS(embedMigrations)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 
-	dbString := os.Getenv("DATABASE_URL")
+	goose.SetBaseFS(embedMigrations)
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load config")
+	}
+	dbString := config.CreateDBConnectionString()
 	if dbString == "" {
-		log.Fatal("DATABASE_URL is not set")
+		log.Fatal().Msg("DATABASE_URL is not set")
 	}
 
 	db, err := sql.Open("pgx", dbString)
 	if err != nil {
-		log.Fatalf("Failed to open DB: %v\n", err)
+		log.Fatal().Err(err).Msg("Failed to open DB")
 	}
 	defer db.Close()
 
 	if err := goose.SetDialect("postgres"); err != nil {
-		log.Fatalf("Failed to set dialect: %v\n", err)
+		log.Fatal().Err(err).Msg("Failed to set dialect")
 	}
 
-	command := "up"
-	if len(os.Args) > 1 {
-		command = os.Args[1]
+	currentVersion, err := goose.GetDBVersion(db)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get current DB version")
 	}
 
-	log.Printf("Running goose command: '%s'...\n", command)
+	log.Info().Int64("current_version", currentVersion).Msg("Starting migration")
 
-	if err := goose.RunContext(context.Background(), command, db, "scripts"); err != nil {
-		log.Fatalf("goose %s failed: %v\n", command, err)
+	if err := goose.Up(db, "scripts"); err != nil {
+		log.Error().Err(err).Msg("Migration failed, attempting rollback")
+
+		if rollbackErr := goose.DownTo(db, "scripts", currentVersion); rollbackErr != nil {
+			log.Fatal().
+				Err(err).
+				AnErr("rollback_error", rollbackErr).
+				Msg("Migration failed AND rollback failed")
+		}
+
+		log.Fatal().Err(err).Msg("Migration failed (rollback succeeded)")
 	}
 
-	log.Println("Migration command completed successfully!")
+	newVersion, _ := goose.GetDBVersion(db)
+	log.Info().
+		Int64("from_version", currentVersion).
+		Int64("to_version", newVersion).
+		Msg("Migration completed successfully")
 }
